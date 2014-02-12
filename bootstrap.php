65f4316a3832c2a419673e919a0d8218a9e6e5ba
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2014-02-12 15:35:22 -0500

    Added the config service provider.

--- bootstrap.php
+++ bootstrap.php
@@ -4,15 +4,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new Silex\Application;
 
-// @see http://silex.sensiolabs.org/doc/providers/url_generator.html
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-
-// @see http://silex.sensiolabs.org/doc/providers/session.html
-$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__ . '/conf/config.yml', array(
+    'root_dir' => __DIR__,
+)));
 
 // @see http://silex.sensiolabs.org/doc/providers/monolog.html
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__ . '/log/acquia-search.log',
 ));
+
+$app->register(new Acquia\Search\Proxy\AcquiaSearchIndexProvider());
 
 return $app;
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+use PSolr\Request as Request;
+
 /* @var $app \Silex\Application */
 $app = require 'bootstrap.php';
 
@@ -13,4 +15,23 @@ $app->get('/', function () use ($app) {
     ));
 });
 
+/**
+ * Autocomplete
+ */
+$app->get('/autocomplete/{query}', function ($query) use ($app) {
+
+    $result = Request\Suggest::factory()
+        ->setQuery($query)
+        ->sendRequest($app['acquia.search'])
+    ;
+
+    return $app->json(array(
+        'status' => 'ok',
+        'suggestions' => $result->suggestions(),
+        'recommendation' => $result->collation(),
+    ));
+});
+
+
 $app->run();
+
